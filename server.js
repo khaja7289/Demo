e4ea409897405b723c60b9d362f5c070c106ee2c
khaja7289 <122887258+khaja7289@users.noreply.github.com>
2024-12-04 18:03:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/homeinside', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
