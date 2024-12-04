d958888a438de64512757a29a4df7a4083c3f5b3
khaja7289 <122887258+khaja7289@users.noreply.github.com>
2024-12-04 17:52:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
